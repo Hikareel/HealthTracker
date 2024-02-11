8675befd23571a74b870dffb7fdb0ebd39dff3ed
Filip Stańczak <filipst166@gmail.com>
2024-02-11 16:21:43 +0100

    added home item

--- HealthTracker/healthtracker.client/src/components/shared/sidebar/sidebarLinks.ts
+++ HealthTracker/healthtracker.client/src/components/shared/sidebar/sidebarLinks.ts
@@ -2,31 +2,36 @@ import { reactive } from "vue";
 
 const Links = reactive({
   1: {
+    name: "Home",
+    link: "/",
+    icon: "home"
+  },
+  2: {
     name: "Meals",
     link: "/diary",
     icon: "restaurant"
   },
-  2: {
+  3: {
     name: "Trainings Planner",
     link: "/planner",
     icon: "fitness_center"
   } ,
-  3: {
+  4: {
     name: "Health Check",
     link: "/health",
     icon: "health_and_safety"
   },
-  4: {
+  5: {
     name: "Goals and Progress",
     link: "/goals",
     icon: "emoji_events"
   },
-  5: {
+  6: {
     name: "Community",
     link: "/community",
     icon: "forum"
   },
-  6: {
+  7: {
     name: "About",
     link: "/about",
     icon: "info"
