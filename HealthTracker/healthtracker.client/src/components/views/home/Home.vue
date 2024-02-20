d91073a4c620f8c67b30ef62de23f5ac03650c0e
DamianSubzda <DamianSubzda@wp.pl>
2024-02-20 12:07:17 +0100

    Home page Fields

--- HealthTracker/healthtracker.client/src/components/views/home/Home.vue
+++ HealthTracker/healthtracker.client/src/components/views/home/Home.vue
@@ -1,12 +1,35 @@
 <template>
   <main class="home-page">
-    <Header title="Home"/>
-    <p>This is a home page</p>
+    <Header title="Home" />
+    <div class="main-view">
+      <HomeMainField class="home-main-field"/>
+      <HomeMainField class="home-main-field"/>
+      <HomeMainField class="home-main-field"/>
+      <HomeMainField class="home-main-field"/>
+      <HomeMainField class="home-main-field"/>
+      <HomeMainField class="home-main-field"/>
+    </div>
+
   </main>
 </template>
 <script lang="ts" setup>
 import Header from '../../shared/header/Header.vue'
+import HomeMainField from './components/HomeMainField.vue'
 </script>
-<style>
-  
+<style scope>
+.home-main-field {
+  margin: 4%;
+}
+
+.main-view {
+  display: grid;
+  text-align: center;
+  grid-template-columns: repeat(2, 1fr);
+}
+
+@media (max-width: 600px) {
+  .main-view {
+    grid-template-columns: 1fr;
+  }
+}
 </style>
---
+++ HealthTracker/healthtracker.client/src/components/views/home/components/HomeMainField.vue
@@ -0,0 +1,55 @@
+<template>
+    <main>
+        <div class="border">
+            <div class="main-box">
+                <div class="top-left">top-left</div>
+                <div class="top-right">top-right</div>
+                <div class="bottom-left">bottom-left</div>
+                <div class="bottom-right">bottom-right</div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+</script>
+<style lang="scss" scope>
+.border {
+    padding: 10px;
+    border-radius: 25px 5px;
+    border: 4px solid #000000;
+    -webkit-box-shadow:5px 5px 15px 5px #ffffff;
+    -moz-box-shadow:5px 5px 15px 5px #ffffff;
+    box-shadow:5px 5px 15px 5px #808080;
+}
+
+.main-box {
+    position: relative;
+    width: 100px;
+    height: 100px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.main-box>div {
+    width: 45%;
+    height: 45%;
+}
+
+.top-left {
+    align-self: flex-start;
+}
+
+.top-right {
+    align-self: flex-start;
+}
+
+.bottom-left {
+    align-self: flex-end;
+}
+
+.bottom-right {
+    align-self: flex-end;
+}
+</style>
