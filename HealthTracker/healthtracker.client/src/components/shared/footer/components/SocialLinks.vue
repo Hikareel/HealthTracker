a879b1b4214bd8695371f4ee6b5523f796447f03
DamianSubzda <DamianSubzda@wp.pl>
2024-02-26 14:36:11 +0100

    Edited footer links events

--- HealthTracker/healthtracker.client/src/components/shared/footer/components/SocialLinks.vue
+++ HealthTracker/healthtracker.client/src/components/shared/footer/components/SocialLinks.vue
@@ -2,7 +2,7 @@
   <div class="social_links">
     <ul>
       <li v-for="link in SocialLinks">
-        <a :href="link.link" target="_blank" class="sl">
+        <a :href="link.link" target="_blank" class="sl" >
           <i :class="link.icon"></i>
           <span class="link_name">{{ link.name }}</span>
         </a>
@@ -30,6 +30,17 @@ import { SocialLinks } from '../data/socialLinks'
     display: grid;
     grid-template-columns: 0.2fr 1fr;
     gap: 10px;
+  }
+  .sl:hover{
+    color: gold;
+    background-color: transparent;
+  }
+  .sl:active{
+    color: blue;
+  }
+  .sl:hover .link_name,
+  .sl:active .link_name {
+    text-decoration: underline;
   }
   .social_links{
     width: 30rem;
